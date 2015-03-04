627e57e9476be270ca6427081c8e592287bfb249
Robert Chady <rchady@sitepen.com>
2015-03-04 10:57:41 -0600

    Import cmds as well

--- playbooks/lookup_plugins/template_src.py
+++ playbooks/lookup_plugins/template_src.py
@@ -32,6 +32,7 @@ from ansible import utils, errors
 import os
 
 from debops import *
+from debops.cmd import *
 
 __author__ = "Robert Chady <rchady@sitepen.com>"
 __copyright__ = "Copyright 2015 by Robert Chady <rchady@sitepen.com>"
